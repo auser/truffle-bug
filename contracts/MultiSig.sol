5d8fbbbc6d0b48110ae51113f7809d0f179c3ca3
Ari <ari@fullstack.io>
2018-04-24 12:23:19 -0400

    Even further simplification

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -1,34 +1,12 @@
-pragma solidity ^0.4.18;
-
-/*
-MultiSig inheritable contract. Allows multiple one-off multisigs.
-
-Usage:
-1. Inherit main contract from MultiSig
-2. Call addMultiSig with the expected signers each time a multisig is needed
-3. Signers can call
-*/
+pragma solidity ^0.4.21;
 
 contract MultiSig {
-
-    /***********************************************************
-    * EVENTS
-    ***********************************************************/
-
     event MultiSigSigned(uint index, address signer);
 
     function sign(uint index)
         public
     {
         emit MultiSigSigned(index, msg.sender);
-    }
-
-    function isComplete(uint index)
-        public
-        constant
-        returns (bool)
-    {
-        return false;
     }
 
 }
--- test/multisig/testMultiSig.js
+++ test/multisig/testMultiSig.js
@@ -1,15 +1,11 @@
 const th = require('../lib/testHelper');
 const should = th.should();
-const {assertThrows} = th;
 
 const MultiSig = artifacts.require('./MultiSig');
 
-contract('MultiSig', ([owner, user1, user2, user3, user4, user5, user6]) => {
-  const threeAccounts = [owner, user1, user2];
-  const threeInvalidAccounts = [owner, user1, 0x0];
+contract('MultiSig', ([owner, user1]) => {
   let multisig;
 
-  before(async () => await th.advanceBlock());
   beforeEach(async () => {
     multisig = await MultiSig.new();
   });
@@ -17,12 +13,10 @@ contract('MultiSig', ([owner, user1, user2, user3, user4, user5, user6]) => {
   describe('events', async () => {
 
     it('fires MultiSigSigned when signed', async () => {
-      const {logs} = await multisig.sign(0, {from: user1, test: true});
+      const {logs} = await multisig.sign(0, {from: user1});
       logs.length.should.equal(1);
       logs[0].event.should.equal('MultiSigSigned');
       logs[0].args.index.should.be.bignumber.equal(0);
-
-      console.log(logs[0].args, user1);
 
       logs[0].args.signer.should.equal(user1);
     });
